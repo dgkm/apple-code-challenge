9d76a272147a44c35bd62b1e6cea19b42a53b380
Keshava Gavigowda <dgkmurthy@gmail.com>
2024-10-22 20:01:03 +0100

    feat: add signature in the data generation

--- backend/cmd/db/main.go
+++ backend/cmd/db/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"github.com/bxcodec/faker/v3"
+	"interview/internal/generator"
 	"log"
 	"math/rand"
-	"net"
 	"os"
 	"time"
 
@@ -17,32 +15,6 @@ const dbFileName = "assets.db"
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-}
-
-func randomDomain() string {
-	return faker.DomainName()
-}
-
-func randomWord() string {
-	return faker.Word()
-}
-
-func randomComment() string {
-	return faker.Sentence()
-}
-
-func randomIP() string {
-	ip := make(net.IP, 4)
-	rand.Read(ip)
-	return ip.String()
-}
-
-func randomOwner() string {
-	return faker.Name()
-}
-
-func randomPort() int {
-	return rand.Intn(65535-1) + 1
 }
 
 func main() {
@@ -62,13 +34,15 @@ func main() {
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			host TEXT NOT NULL,
 			comment TEXT,
-			owner TEXT NOT NULL
+			owner TEXT NOT NULL,
+			signature TEXT
 		);
 
 		CREATE TABLE ips (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			asset_id INTEGER,
 			address TEXT NOT NULL,
+			signature TEXT,
 			FOREIGN KEY(asset_id) REFERENCES assets(id)
 		);
 
@@ -76,6 +50,7 @@ func main() {
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			asset_id INTEGER,
 			port INTEGER NOT NULL,
+			signature TEXT,
 			FOREIGN KEY(asset_id) REFERENCES assets(id)
 		);`
 
@@ -85,19 +60,19 @@ func main() {
 	}
 
 	// Prepare statements for inserts
-	insertAssetStmt, err := db.Prepare("INSERT INTO assets(host, comment, owner) VALUES(?, ?, ?)")
+	insertAssetStmt, err := db.Prepare("INSERT INTO assets(host, comment, owner, signature) VALUES(?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer insertAssetStmt.Close()
 
-	insertIPStmt, err := db.Prepare("INSERT INTO ips(asset_id, address) VALUES(?, ?)")
+	insertIPStmt, err := db.Prepare("INSERT INTO ips(asset_id, address, signature) VALUES(?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer insertIPStmt.Close()
 
-	insertPortStmt, err := db.Prepare("INSERT INTO ports(asset_id, port) VALUES(?, ?)")
+	insertPortStmt, err := db.Prepare("INSERT INTO ports(asset_id, port, signature) VALUES(?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -107,10 +82,8 @@ func main() {
 	totalEntries := 10000
 	for i := 0; i < totalEntries; i++ {
 		// Insert asset
-		host := fmt.Sprintf("%s.%s", randomWord(), randomDomain())
-		comment := randomComment()
-		owner := randomOwner()
-		result, err := insertAssetStmt.Exec(host, comment, owner)
+		asset := generator.GenerateAsset()
+		result, err := insertAssetStmt.Exec(asset.Host, asset.Comment, asset.Owner, asset.Signature)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -122,8 +95,8 @@ func main() {
 
 		// Insert related IPs
 		for j := 0; j < rand.Intn(3)+1; j++ { // Each asset has 1-3 IPs
-			ip := randomIP()
-			_, err := insertIPStmt.Exec(assetID, ip)
+			ip := generator.GenerateIP()
+			_, err := insertIPStmt.Exec(assetID, ip.Address, ip.Signature)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -131,14 +104,14 @@ func main() {
 
 		// Insert related Ports
 		for j := 0; j < rand.Intn(5)+1; j++ { // Each asset has 1-5 ports
-			port := randomPort()
-			_, err := insertPortStmt.Exec(assetID, port)
+			port := generator.GeneratePort()
+			_, err := insertPortStmt.Exec(assetID, port.Port, port.Signature)
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
 
-		if i%1000 == 0 {
+		if i%100 == 0 {
 			log.Printf("Inserted %d/%d entries", i, totalEntries)
 		}
 	}
