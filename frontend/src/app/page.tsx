1fd8caa15f1da24930d5ed275194d481dcf4809c
Keshava Gavigowda <dgkmurthy@gmail.com>
2024-10-24 11:14:33 +0100

    feat: original page showing total records

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,8 +4,12 @@ import * as React from 'react';
 
 import { PageSection } from '@/components/custom/page/PageSection';
 
+import { Changes } from '@/improvements/Changes';
+
 export default function HomePage() {
   return (
-    <PageSection title='Identified or Implemented Changes'>Changes</PageSection>
+    <PageSection title='Identified Improvements or Implemented Changes'>
+      <Changes />
+    </PageSection>
   );
 }
